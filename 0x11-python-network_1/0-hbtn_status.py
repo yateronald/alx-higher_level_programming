9b925685d3049587fed2cb9469597e6d06f0c179
yateronald <yateronald@gmail.com>
2022-09-25 15:52:31 -0700

    networking 2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """ Fetches basic request with urlib"""
 from urllib import request
 
-with request.urlopen("https://intranet.hbtn.io/status") as response:
+with request.urlopen("https://alx.intranet.hbtn.io/status") as response:
     r = response.read()
     print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
           .format(type(r), r, r.decode('utf-8')))
