0f795209ffbab2de65be43749ee5f1fe10b35126
yateronald <yateronald@gmail.com>
2022-09-25 15:47:56 -0700

    networking 2

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curls post request with 2 params
-curl -s -X POST -d "email=test@gmail&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
